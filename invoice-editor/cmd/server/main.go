998c7b82d8f8d2ac0c9c12b4d78be2c5241145c4
orelAbecassis <orelabk6.oa.oa@gmail.com>
2025-02-04 17:44:40 +0100

    add dockerfile and docker compose

--- invoice-editor/cmd/server/main.go
+++ invoice-editor/cmd/server/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "invoice-editor/internal/api"
+	"invoice-editor/internal/api"
+	"log"
+	"net/http"
 )
 
 func main() {
-    router := api.NewRouter()
-    
-    log.Println("Starting server on :8080")
-    if err := http.ListenAndServe(":8080", router); err != nil {
-        log.Fatal(err)
-    }
+	router := api.NewRouter()
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
 }
